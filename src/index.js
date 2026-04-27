0b0a4c39b936d17a8e93e90b54bddb94ed204d27
Sean Tant <seantant@umich.edu>
2026-04-27 15:38:30 -0400

    Cleaned up unused files. Created getWeather function

--- src/index.js
+++ src/index.js
@@ -1,33 +1,27 @@
 import "./style.css";
-import homePage from "./homePage.js";
-import menuPage from "./menuPage.js";
-import contactPage from "./contactPage.js";
 
-homePage();
+const apiKey = "EF4EBER2PES3PG44HZHE9CRQY";
+const urlPrefix =
+  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
 
-function clearContent() {
-    const contentDiv = document.querySelector("#content");
 
-    while (contentDiv.firstChild) {
-        contentDiv.removeChild(contentDiv.lastChild);
-    }
+async function getWeather(location) {
+    let url =
+      urlPrefix +
+      location +
+      "?key=" +
+      apiKey;
+
+    let response = await fetch(url);
+
+    console.log("Requested data:")
+    console.log(data);
+
+    return response;
 }
 
-const homeBtn = document.querySelector("#home");
-const menuBtn = document.querySelector("#menu");
-const contactBtn = document.querySelector("#contact");
+function parseData(response) {
+    
+}
 
-homeBtn.addEventListener("click", () => {
-    clearContent();
-    homePage();
-});
-
-menuBtn.addEventListener("click", () => {
-    clearContent();
-    menuPage();
-});
-
-contactBtn.addEventListener("click", () => {
-    clearContent();
-    contactPage();
-});
+getWeather("berlin");
